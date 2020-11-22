32f84dab809e96b507e5dd4b5514d186d1553a24
Vany Liu <vany8462@gmail.com>
2020-11-22 11:22:10 +0800

    reopen a closed account

--- includes/form_handlers/login_handler.php
+++ includes/form_handlers/login_handler.php
@@ -15,6 +15,12 @@ if(isset($_POST['login_button'])){
         $row = mysqli_fetch_array($check_database_query);
         $username = $row['username'];
 
+        $user_closed_query = mysqli_query($con,"SELECT * FROM users WHERE email='$email' AND user_closed='yes'");
+        if(mysqli_num_rows($user_closed_query) == 1){
+            //user simply login again and we will reopen the account
+            $reopen_account = mysqli_query($con, "UPDATE users SET user_closed='no' WHERE email='$email'");
+        }
+
         $_SESSION['username'] = $username;
         header("Location: index.php"); // Redirect the page to index.php
         exit();
